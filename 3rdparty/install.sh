552e4aac7debe83e0f67c90ec5c62eaac8135309
tmartinez <tmartinez69009@gmail.com>
2017-05-23 16:47:27 +0200

    Install missing packages

--- 3rdparty/install.sh
+++ 3rdparty/install.sh
@@ -2,7 +2,9 @@
 touch /tmp/dependancy_wifip_in_progress
 echo 0 > /tmp/dependancy_wifip_in_progress
 echo "########### Installation en cours ##########"
+sudo apt-get install firmware-ralink wpasupplicant wireless-tools
+echo 50 > /tmp/dependancy_wifip_in_progress
 sudo connmanctl enable wifi
 echo 100 > /tmp/dependancy_wifip_in_progress
-echo "########### Fin ##########"
+echo "########### Fin - Merci de redémarrer votre box afin d'activer le Wifi ##########"
 rm /tmp/dependancy_wifip_in_progress
